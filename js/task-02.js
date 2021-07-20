43836c9cb07038822e759ae66a1e255c9a5eb9da
Anton Kovalchuck <antonkovalchuck@gmail.com>
2021-07-20 16:04:27 +0300

    task 2 upd

--- js/task-02.js
+++ js/task-02.js
@@ -10,10 +10,15 @@ const ingredients = [
 //для каждого элемента массива ingredients создаст отдельный li, 
 //после чего вставит все li за одну операцию в список ul.ingredients
 
-let list = document.createElement('ul');
-ingredients.forEach(ingredient => {
-  let temp = document.createElement('li');
-  temp.textContent = ingredient;
-  list.appendChild(temp);
-})
-console.log('list :>> ', list);
+let list = document.querySelector('#ingredients');
+// ingredients.forEach(ingredient => {
+//   let temp = document.createElement('li');
+//   temp.textContent = ingredient;
+//   list.appendChild(temp);
+// })
+
+const addIngredient = ingredients.map( ingredient => {
+  const ingredientItem = `<li>${ingredient}</li>`;
+  return ingredientItem;
+});
+list.insertAdjacentHTML("beforeend", addIngredient.join(''));
